c3bf50e02bdefe5179cf841b9a43b3703991ca3c
Debayan Pradhan <pradhandebayan@gmail.com>
2024-04-16 16:40:21 +0530

    updated url

--- src/constants/baseurl.constant.ts
+++ src/constants/baseurl.constant.ts
@@ -1,2 +1,1 @@
-// export const BASE_URL = "http://localhost:7000/api/v1"
-export const BASE_URL = "https://treasurehuntapi-lbagduga2a-uc.a.run.app/api/v1"
+export const BASE_URL = "https://treasurehuntapi-lbagduga2a-uc.a.run.app/api/v1/"
